26bc0f5cb76fddf7b97c08a0f3907c12615f7367
IDGS-801-22002343 <80266@alumnos.utleon.edu.mx>
2025-11-04 19:08:28 -0600

    Primer Commit

---
+++ Migrations/20251105002357_Prueba2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_RSV.Migrations
+{
+    /// <inheritdoc />
+    public partial class Prueba2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prueba");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prueba",
+                columns: table => new
+                {
+                    AvisoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prueba", x => x.AvisoID);
+                });
+        }
+    }
+}
